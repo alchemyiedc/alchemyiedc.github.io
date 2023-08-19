44373818221855bc5d0c410655138b4fdf77ae74
havisvh <havisvh35@gmail.com>
2023-08-19 23:08:31 +0530

    The SuperGallery Update

--- README.md
+++ README.md
@@ -25,40 +25,4 @@ _you can duplicate `temp_file.html` and rename it to your page name and start wo
 
 After Making changes to your local machine, push the changes to your forked repository and make a pull request to the main repository.
 
-## Working with **Tabbed Web App**
-
----
-
-example usage
-
-```html
-<!-- Page Content Begin -->
-<h1 style="text-align: center;">My Awesome Title</h1>
-<p>My Super Paragraph</p>
-<hr />
-<p>Some More Text</p>
-
-#S{ console.log("Page Has Been Rendered!!");
-     a = 2;
-     b = 3;
-    console.log(a+b);
-    //result will be 5 
-    //you can use any javascript code here 
-    //and it will be executed when the page is rendered
-    jump();
-}
-
-#F{
-    // jump is the function name and anything between : and ; are the
-    function body, it can be called anywhere in the code after page is rendered
-    //here each new line is a new line in the function body
-
-    jump: console.log("jumped")
-    console.log("This is a function-- it is deprecated but still usable")
-    ;
-}
-
-<!-- Page Content End -->
-
-
-```
+_Please dont mess with the website structure. for the love of god_
---
+++ SuperGallery.js
@@ -0,0 +1,140 @@
+// SuperGallery JS V1.0 is copyrighted to Havis V H under the brand HVH
+// But Have fun Using it!! :D
+const styles = [
+    'color: white',
+
+    'font-size: 30px',
+
+    'font-family:"Outfit", sans-serif ',
+    'font-weight: bold',
+
+].join(';');
+const styles2 = [
+    'color: #5656f6',
+    'background: #111',
+    'font-size: 30px',
+    'border-radius: 12px 0px 12px 0px',
+    'padding: 10px',
+    'font-family:"Outfit", sans-serif ',
+    'font-weight: bold',
+    'margin:12px'
+].join(';');
+
+console.log("%cHello There!!!", styles);
+console.log("%cthis Website uses SuperGallery from %cHVH", styles, styles2);
+
+function openGallery() {
+    document.getElementById('galleryWindow').style.display = 'flex';
+    setTimeout(() => {
+        if (window.innerWidth > 750) {
+            document.getElementById('galleryWindow').style.height = '90vh';
+            document.getElementById('galleryWindow').style.width = '90vw';
+            document.getElementById('galleryWindow').style.top = '50%';
+            document.getElementById('galleryWindow').style.opacity = '100%';
+        }
+        else {
+            document.getElementById('galleryWindow').style.height = '100vh';
+            document.getElementById('galleryWindow').style.width = '100vw';
+
+            document.getElementById('galleryWindow').style.top = '50%';
+            document.getElementById('galleryWindow').style.opacity = '100%';
+        }
+    }, 100);
+}
+
+function closeGallery() {
+    document.getElementById('galleryWindow').style.height = '20vh';
+    document.getElementById('galleryWindow').style.width = '30vw';
+    document.getElementById('galleryWindow').style.top = '70%';
+    document.getElementById('galleryWindow').style.opacity = '0%';
+    setTimeout(() => {
+        document.getElementById('galleryWindow').style.display = 'none';
+
+    }, 400);
+}
+
+_isCarouselOpen = false;
+_imgIndex = 0;
+
+window.addEventListener("message", function (event) {
+    picsDecrypt = atob(event.data);
+    picsJson = JSON.parse(picsDecrypt);
+    if (picsJson.event == 'loaded') {
+        pics = picsJson.data;
+        document.getElementById('carouselImage').style.background = 'url(' + pics[0] + ') no-repeat center center';
+        document.getElementById('carouselImage').style.backgroundSize = 'contain';
+        var img = new Image();
+        img.src = pics[0];
+        imgLoader(img);
+    }
+    else if (picsJson.event == "target") {
+        _imgIndex = picsJson.data;
+        document.getElementById('carouselImage').style.background = 'url(' + pics[_imgIndex] + ') no-repeat center center';
+        document.getElementById('carouselImage').style.backgroundSize = 'contain';
+        var img = new Image();
+        img.src = pics[_imgIndex];
+        imgLoader(img);
+        Carousel();
+    }
+});
+
+
+carouseL = document.getElementById('phoato');
+
+function imgLoader(img) {
+    img.onload = function () {
+        var originalWidth = img.naturalWidth;
+        var originalHeight = img.naturalHeight;
+        var targetWidth = 400;
+        if (window.innerWidth < 750) {
+            targetWidth = window.innerWidth - 120;
+        }
+        var aspectRatio = originalWidth / originalHeight;
+        var newHeight = targetWidth / aspectRatio;
+        document.getElementById('carouselImage').style.height = newHeight + 'px';
+        document.getElementById('carouselImage').style.width = targetWidth + 'px';
+    };
+    document.getElementById('imageCounter').innerHTML = '1/' + pics.length;
+}
+
+function Carousel() {
+    if (_isCarouselOpen) {
+        // carouseL.style.height = '0vh';
+        carouseL.style.opacity = '0%';
+        setTimeout(() => {
+            carouseL.style.display = 'none';
+            document.body.style.overflow = 'auto';
+        }, 400);
+        _isCarouselOpen = false;
+    } else {
+
+        carouseL.style.display = 'flex';
+        document.body.style.overflow = 'hidden';
+        setTimeout(() => {
+            // carouseL.style.height = '100vh';
+            carouseL.style.opacity = '100%';
+        }, 100);
+        _isCarouselOpen = true;
+    }
+}
+
+function nextImage() {
+    if (_imgIndex < pics.length - 1) {
+        _imgIndex++;
+        document.getElementById('carouselImage').style.background = 'url(' + pics[_imgIndex] + ') no-repeat center center';
+        document.getElementById('carouselImage').style.backgroundSize = 'contain';
+        var img = new Image();
+        img.src = pics[_imgIndex];
+        imgLoader(img);
+    }
+}
+function prevImage() {
+    if (_imgIndex > 0) {
+        _imgIndex--;
+        document.getElementById('carouselImage').style.background = 'url(' + pics[_imgIndex] + ') no-repeat center center';
+        document.getElementById('carouselImage').style.backgroundSize = 'contain';
+        var img = new Image();
+        img.src = pics[_imgIndex];
+        imgLoader(img);
+    }
+}
